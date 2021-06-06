ec77b11874b919afaa21c6d56ed06628de85d571
hoho <37791965+oekaki-hoho-ron@users.noreply.github.com>
2021-06-06 22:36:33 +0900

    SDカードのtable試作

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -5,8 +5,12 @@
 export default {
   '__react_static_root__/src/pages/404.js': require('__react_static_root__/src/pages/404.js').default,
 '__react_static_root__/src/pages/blog.js': require('__react_static_root__/src/pages/blog.js').default,
+'__react_static_root__/src/pages/choose/fluorescent-light.js': require('__react_static_root__/src/pages/choose/fluorescent-light.js').default,
+'__react_static_root__/src/pages/choose/glow-starter.js': require('__react_static_root__/src/pages/choose/glow-starter.js').default,
 '__react_static_root__/src/pages/choose/index.js': require('__react_static_root__/src/pages/choose/index.js').default,
 '__react_static_root__/src/pages/choose/lan-cable.js': require('__react_static_root__/src/pages/choose/lan-cable.js').default,
+'__react_static_root__/src/pages/choose/sd-card.js': require('__react_static_root__/src/pages/choose/sd-card.js').default,
+'__react_static_root__/src/pages/choose/shaver.js': require('__react_static_root__/src/pages/choose/shaver.js').default,
 '__react_static_root__/src/pages/choose/wifi-router.js': require('__react_static_root__/src/pages/choose/wifi-router.js').default,
 '__react_static_root__/src/pages/disposal/index.js': require('__react_static_root__/src/pages/disposal/index.js').default,
 '__react_static_root__/src/pages/index.js': require('__react_static_root__/src/pages/index.js').default,
---
+++ src/pages/choose/sd-card.js
@@ -0,0 +1,390 @@
+import React from 'react'
+
+export default () => (
+<div>
+
+
+<h2>SDカード</h2>
+
+<h3>形状 ~フォームファクタ~</h3>
+<p>フォームファクタとはSDカードの形状、主に大きさの違いを指す。</p>
+<table border="1px">
+  <tr>
+    <th>形状</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+
+  <tr>
+    <td>SD</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>miniSD</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>microSD</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+<h3>保存容量 ~ストレージ~</h3>
+<table border="1px">
+  <tr>
+    <th>保存容量</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+
+  <tr>
+    <td>標準</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>HC</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>XC</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>UC</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+<h3>データ転送速度 ~バス・インターフェース・スピード~</h3>
+<h4>DS</h4>
+<h4>HS</h4>
+<h4>UHS-Ⅰ</h4>
+<h4>UHS-Ⅱ</h4>
+<h4>UHS-Ⅲ</h4>
+<h4>SD Express</h4>
+
+<table border="1px">
+  <tr>
+    <th>データ転送速度</th>
+    <th>DS</th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+
+  <tr>
+    <td>HS</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+<h3>映像データ書き込み最低速度 ~スピードクラス~</h3>
+<h4></h4>
+<h4></h4>
+<h4></h4>
+
+<table border="1px">
+  <tr>
+    <th>クラス</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+
+  <tr>
+    <td>UHS-Ⅰ</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>UHS-Ⅱ</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>UHS-Ⅲ</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>SD Express</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+
+<h3>ランダムアクセス性能 ~アプリケーション・パフォーマンス・クラス~</h3>
+<table border="1px">
+  <tr>
+    <th>クラス</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+
+  <tr>
+    <td>A1</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td>A2</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+<h3>ファイル形式 ~アプリケーション・フォーマット~</h3>
+<h3></h3>
+<h3></h3>
+
+<table border="1px">
+  <tr>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+
+
+
+<table>
+	<tbody>
+		<tr>
+			<th>最低保証速度</th>
+			<th>スピードクラス</th>
+			<th>UHSスピードクラス</th>
+			<th>ビデオスピードクラス</th>
+			<th colspan="4">ビデオフォーマット</th>
+		</tr>
+		<tr>
+			<td>90MB/s</td>
+			<th rowspan="3"></th>
+			<th rowspan="2"></th>
+			<td>V90</td>
+			<td rowspan="3">8K</td>
+			<th></th>
+			<th rowspan="2"></th>
+			<th rowspan="3"></th>
+		</tr>
+		<tr>
+			<td>60MB/s</td>
+			<td>V60</td>
+			<td rowspan="4">4K</td>
+		</tr>
+		<tr>
+			<td>30MB/s</td>
+			<td>U3</td>
+			<td>V30</td>
+			<td rowspan="4">HD・フルHD</td>
+		</tr>
+		<tr>
+			<td>10MB/s</td>
+			<td>C10</td>
+			<td>U1</td>
+			<td>V10</td>
+			<th rowspan="4"></th>
+			<td rowspan="5">スタンダード</td>
+		</tr>
+		<tr>
+			<td>6MB/s</td>
+			<td>C6</td>
+			<th rowspan="3"></th>
+			<td>V6</td>
+		</tr>
+		<tr>
+			<td>4MB/s</td>
+			<td>C4</td>
+			<th rowspan="2"></th>
+			<th rowspan="2"></th>
+		</tr>
+		<tr>
+			<td>2MB/s</td>
+			<td>C2</td>
+			<th></th>
+		</tr>
+	</tbody>
+</table>
+
+</div>
+)
